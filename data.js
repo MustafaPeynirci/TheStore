26c96d4b1cbc0e456da53ec6c411533bfbaec05e
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-03-29 03:19:47 +0300

    json-server

---
+++ data.js
@@ -0,0 +1,54 @@
+module.exports = function () {
+  return {
+    products: [
+      {
+        id: 1,
+        name: "iPhone 7 Plus",
+        price: 4490,
+        imageUrl: "i7.png",
+        description: "The iPhone 7 Plus",
+      },
+      {
+        id: 2,
+        name: "iPhone 8 Plus",
+        price: 6140,
+        imageUrl: "i8.png",
+        description: "The iPhone 8 Plus",
+      },
+      {
+        id: 3,
+        name: "iPhone X",
+        price: 8410,
+        imageUrl: "iX.png",
+        description: "The iPhone X",
+      },
+      {
+        id: 4,
+        name: "iPhone 11",
+        price: 13570,
+        imageUrl: "i11.png",
+        description: "The iPhone 11",
+      },
+      {
+        id: 5,
+        name: "iPhone 12",
+        price: 17230,
+        imageUrl: "i12.png",
+        description: "The iPhone 12",
+      },
+      {
+        id: 6,
+        name: "iPhone 13",
+        price: 24890,
+        imageUrl: "i13.png",
+        description: "The iPhone 13",
+      },
+    ],
+    categories: [
+      { id: 1, name: "Telephone" },
+      { id: 2, name: "Computer" },
+      { id: 3, name: "Electronic" },
+    ],
+    orders: [],
+  };
+};
