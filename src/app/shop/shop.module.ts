2f3dd8ed31de5219311e9c02174634b08e25fdfe
Mustafa Peynirci <mustafapeynircigithub@gmail.com>
2023-03-30 06:33:09 +0300

    navbar component

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -3,11 +3,12 @@ import { ModelModule } from './../model/model.module';
 import { NgModule } from "@angular/core";
 import { ShopComponent } from './shop.component';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
     imports: [ModelModule, BrowserModule, FormsModule],
     providers: [],
-    declarations: [ShopComponent],
+    declarations: [ShopComponent, NavbarComponent],
     exports: [ShopComponent]
 })
 export class ShopModule { }
